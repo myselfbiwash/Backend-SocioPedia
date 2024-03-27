89155dce0cd7c4589f5062ec028bbb89e75ad7a3
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-03-27 17:39:57 +0545

    Add commentPost function to handle commenting on posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -71,3 +71,17 @@ export const likePost = async (req, res) => {
     res.status(404).json({ message: err.message });
   }
 };
+
+export const commentPost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { comment, userId } = req.body;
+    const post = await Post.findById(id);
+    post.comments.push({ userId, comment });
+    const updatedPost = await Post.findByIdAndUpdate(id, post, { new: true });
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
